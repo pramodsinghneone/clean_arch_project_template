57b9356542e706cc6fe62663e6e89f3e11678762
PramodSingh <pramodsinghneo@gmail.com>
2022-09-21 13:06:08 +0530

    files updated

--- lib/feature/core/base_network.dart
+++ lib/feature/core/base_network.dart
@@ -92,4 +92,22 @@ class NetworkService {
     // headers.addAll(headersLocal);
     return headers;
   }
+
+//   dynamic responseCommon(http.Response response) {
+//   switch (response.statusCode) {
+//     case 200:
+//       var responseJson = json.decode(response.body.toString());
+//       print(responseJson);
+//       return responseJson;
+//     case 400:
+//       throw BadRequestException(response.body.toString());
+//     case 401:
+//     case 403:
+//       throw UnauthorisedException(response.body.toString());
+//     case 500:
+//     default:
+//       throw FetchDataException(
+//           'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+//   }
+// }
 }
--- lib/feature/core/error/exceptions.dart
+++ lib/feature/core/error/exceptions.dart
@@ -7,3 +7,26 @@ class InternetException implements Exception {
   final String? error;
   InternetException({this.error});
 }
+
+
+// class AppException implements Exception {
+//   final _message;
+//   final _prefix;
+// AppException([this._message, this._prefix]);
+// String toString() {
+//     return "$_prefix$_message";
+//   }
+// }
+// class FetchDataException extends AppException {
+//   FetchDataException([String? message])
+//       : super(message, "Error During Communication: ");
+// }
+// class BadRequestException extends AppException {
+//   BadRequestException([message]) : super(message, "Invalid Request: ");
+// }
+// class UnauthorisedException extends AppException {
+//   UnauthorisedException([message]) : super(message, "Unauthorised: ");
+// }
+// class InvalidInputException extends AppException {
+//   InvalidInputException([String? message]) : super(message, "Invalid Input: ");
+// }
